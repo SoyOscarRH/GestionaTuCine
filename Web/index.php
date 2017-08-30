fb09a39368c770371bddc11f2233a663202b48ed
Oscar Andrés Rosas <soyoscarrh@gmail.com>
2017-08-30 08:20:45 -0500

    Initial Setup
    Create a simple webpage

---
+++ Web/index.php
@@ -0,0 +1,347 @@
+<!DOCTYPE html>
+<HTML>
+
+
+    <!--  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
+    <!--  +++++++++++++++++++++++++++++++++++++++       HEADINGS            ++++++++++++++++++++++++++++++++++++++ -->
+    <!--  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
+    <HEAD>
+
+        <!--  +++++++++++++++++++++++++++++++++++++++++++++++++ -->
+        <!--  +++++++++++++++   PAGE INFO   +++++++++++++++++++ -->
+        <!--  +++++++++++++++++++++++++++++++++++++++++++++++++ -->
+
+            <!-- Page Info -->
+            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />  
+            <!-- How we should see it -->
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <!-- Color in Android Header -->
+            <meta name="theme-color" content="#2196F3">
+
+            <title>Cine</title>
+
+        <!--  +++++++++++++++++++++++++++++++++++++++++++++++++ -->
+        <!--  +++++++++++++++   PAGE STYLE  +++++++++++++++++++ -->
+        <!--  +++++++++++++++++++++++++++++++++++++++++++++++++ -->
+
+            <!-- Icon of the Page -->
+            <link href="MediaAndStyle/favicon.ico" rel="shortcut icon" type="image/x-icon"/>
+
+            <!-- Google Material Fonts -->
+            <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+            <!-- Materialize -->
+            <link href="Style/CSS/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+            <link href="Style/CSS/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+
+    </HEAD>
+
+
+        
+
+        
+
+
+<!--  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
+<!--  +++++++++++++++++++++++++++++++++++++++       BODY                ++++++++++++++++++++++++++++++++++++++ -->
+<!--  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
+<BODY>
+
+
+    <!--  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
+    <!--  +++++++++++++++++    NAVITAGION BAR       +++++++++++++++++++++ -->
+    <!--  ++++++++++++++++++++++++*++++++++++++++++++++++++++++++++++++++ -->
+    <header>
+        <div class="navbar-fixed">
+        <nav class="teal lighten-3">
+        <div class="nav-wrapper container">
+            
+            <!-- Name of the Page -->
+            <a href="#!" class="brand-logo white-text"><i class="material-icons">movie</i>Logo</a>
+
+            <!-- Menu for Mobile -->
+            <a href="#" data-activates="mobile-demo" class="button-collapse white-text">
+                <i class="material-icons">menu</i>
+            </a>
+             
+            <!-- Links for Normal Web -->
+            <ul class="right hide-on-med-and-down">
+                <li> <a class="white-text" href="a.html">Opcion</a> </li>
+                <li> <a class="white-text" href="a.html">Opcion</a> </li>
+                <li> <a class="white-text" href="a.html">Opcion</a> </li>
+                <li> <a class="white-text" href="a.html">Opcion</a> </li>
+            </ul>
+
+        </div>
+        </nav>
+        </div>
+
+        <!-- Links for Mobile Web -->
+        <ul class="side-nav" id="mobile-demo">
+            <li> <a href="a.html">Opcion</a>    </li>
+            <li> <a href="a.html">Opcion</a>    </li>
+            <li> <a href="a.html">Opcion</a>    </li>
+            <li> <a href="a.html">Opcion</a>    </li>
+        </ul>
+
+    </header>
+
+
+
+
+
+    <!--  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
+    <!--  ++++++++++++++   PARALLAX CONTAINER       +++++++++++++++++++++ -->
+    <!--  ++++++++++++++++++++++++*++++++++++++++++++++++++++++++++++++++ -->
+    <div class="parallax-container valign-wrapper">
+        
+        <!-- ===== THE IMAGE ===== -->
+        <div class="parallax"><img src="Media/Movies1.jpg"></div>
+
+        <!-- ===== CONTAINER ===== -->
+        <div class="container center">
+            
+            <h1 class="header teal-text text-lighten-2">Titulo Estupido</h1>
+
+            <h5 class="header col s12 light">Mini Titulo Estupido</h5>
+
+            <!-- ===== BUTTON ===== -->
+            <center>
+            <a  href  = "http://materializecss.com/getting-started.html"
+                id    = "download-button"
+                class = "btn-large waves-effect waves-light teal lighten-1">
+                Get Started
+            </a>
+            </center>
+
+        </div>
+
+    </div>
+
+
+    <!--  ++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
+    <!--  ++++++++++++++   INFORMATION   +++++++++++++++++++++ -->
+    <!--  ++++++++++++++++++++++++*+++++++++++++++++++++++++++ -->
+    <div class="container section">
+    <div class="row">
+           
+        <!--  ============================ -->
+        <!--  =========  COLUMN   ======== -->
+        <!--  ============================ -->
+        <div class="col s12 m4">
+        <div class="icon-block">
+
+            <!--  =========  Icon   ======== -->
+            <h2 class="center brown-text"><i class="material-icons">flash_on</i></h2>
+
+            <!--  =========  Title   ======== -->
+            <h5 class="center">MiniTitulo1</h5>
+
+            <!--  =========  Text   ======== -->
+            <p class="light" align="justify">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Nullam scelerisque id nunc nec volutpat. Etiam pellentesque
+                tristique arcu, non consequat magna fermentum ac. Cras ut ultricies
+                eros. Maecenas eros justo, ullamcorper a sapien id, viverra ultrices
+                eros. Morbi sem neque, posuere et pretium eget, bibendum
+            </p>
+
+        </div>
+        </div>
+
+        <!--  ============================ -->
+        <!--  =========  COLUMN   ======== -->
+        <!--  ============================ -->
+        <div class="col s12 m4">
+        <div class="icon-block">
+
+            <!--  =========  Icon   ======== -->
+            <h2 class="center brown-text"><i class="material-icons">settings</i></h2>
+
+            <!--  =========  Title   ======== -->
+            <h5 class="center">MiniTitulo1</h5>
+
+            <!--  =========  Text   ======== -->
+            <p class="light" align="justify">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Nullam scelerisque id nunc nec volutpat. Etiam pellentesque
+                tristique arcu, non consequat magna fermentum ac. Cras ut ultricies
+                eros. Maecenas eros justo, ullamcorper a sapien id, viverra ultrices
+                eros. Morbi sem neque, posuere et pretium eget, bibendum
+            </p>
+
+        </div>
+        </div>
+
+        <!--  ============================ -->
+        <!--  =========  COLUMN   ======== -->
+        <!--  ============================ -->
+        <div class="col s12 m4">
+        <div class="icon-block">
+
+            <!--  =========  Icon   ======== -->
+            <h2 class="center brown-text"><i class="material-icons">group</i></h2>
+
+            <!--  =========  Title   ======== -->
+            <h5 class="center">MiniTitulo1</h5>
+
+            <!--  =========  Text   ======== -->
+            <p class="light" align="justify">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Nullam scelerisque id nunc nec volutpat. Etiam pellentesque
+                tristique arcu, non consequat magna fermentum ac. Cras ut ultricies
+                eros. Maecenas eros justo, ullamcorper a sapien id, viverra ultrices
+                eros. Morbi sem neque, posuere et pretium eget, bibendum
+            </p>
+
+        </div>
+        </div>
+
+    </div>
+    </div>
+
+
+    <!--  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
+    <!--  ++++++++++++++   PARALLAX CONTAINER 2     +++++++++++++++++++++ -->
+    <!--  ++++++++++++++++++++++++*++++++++++++++++++++++++++++++++++++++ -->
+    <div class="parallax-container valign-wrapper">
+        
+        <!-- ===== THE IMAGE ===== -->
+        <div class="parallax"><img src="Media/Movies2.jpg"></div>
+
+        <h5 class="container center header col s12 light">
+            Un texto bonito :D
+        </h5>
+
+        </div>
+
+    </div>
+
+
+
+
+    <!--  ++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
+    <!--  ++++++++++++++   INFORMATION   +++++++++++++++++++++ -->
+    <!--  ++++++++++++++++++++++++*+++++++++++++++++++++++++++ -->
+    <div class="container section">
+        <div class="row">
+        <div class="col s12 center">
+
+            <!--  =========  Title   ======== -->
+            <h3><i class="mdi-content-send brown-text"></i></h3>
+            <h4>Llamame chiquito</h4>
+          
+            <p class="light" align="justify">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Nullam scelerisque id nunc nec volutpat. Etiam pellentesque
+                tristique arcu, non consequat magna fermentum ac. Cras ut
+                ultricies eros. Maecenas eros justo, ullamcorper a sapien id,
+                viverra ultrices eros. Morbi sem neque, posuere et pretium eget,
+                bibendum sollicitudin lacus. Aliquam eleifend sollicitudin diam,
+                eu mattis nisl maximus sed. Morbi massa odio, condimentum sed
+                ipsum ac, gravida ultrices erat. Nullam eget dignissim mauris, non
+                tristique erat. Vestibulum ante ipsum primis in faucibus orci luctus
+                et ultrices posuere cubilia Curae
+            </p>
+
+        </div>
+        </div>
+
+    </div>
+
+
+
+
+
+    <!--  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
+    <!--  ++++++++++++++   PARALLAX CONTAINER 2     +++++++++++++++++++++ -->
+    <!--  ++++++++++++++++++++++++*++++++++++++++++++++++++++++++++++++++ -->
+    <div class="parallax-container valign-wrapper">
+        
+        <!-- ===== THE IMAGE ===== -->
+        <div class="parallax"><img src="Media/Movies3.jpg"></div>
+
+        <h5 class="container center header col s12 light">
+            Un texto bonito :D
+        </h5>
+
+        </div>
+
+    </div>
+
+
+
+    
+    <!--  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
+    <!--  ++++++++++++++           FOOTER           +++++++++++++++++++++ -->
+    <!--  ++++++++++++++++++++++++*++++++++++++++++++++++++++++++++++++++ -->
+    <footer class="page-footer teal">
+        
+        <div class="row container">
+        
+            <!-- ========================= -->
+            <!-- =======   ABOUT US   ==== -->
+            <!-- ========================= -->
+            <div class="col l6 s12">
+                <!--  =========  Title   ======== -->
+                <h5 class="white-text">Sobre Nosotros</h5>
+
+                <!--  =========  Text   ======== -->
+                <p class="grey-text text-lighten-4">
+                    Somos pro :v
+                </p>
+            </div>
+
+
+            <!-- ========================= -->
+            <!-- ==   ADITIONAL LINKS   == -->
+            <!-- ========================= -->
+            <div class="col l3 s12">
+                <h5 class="white-text">Paginas Adicionales</h5>
+                <ul>
+                    <li><a class="white-text" href="#!">Link 1</a></li>
+                    <li><a class="white-text" href="#!">Link 2</a></li>
+                    <li><a class="white-text" href="#!">Link 3</a></li>
+                    <li><a class="white-text" href="#!">Link 4</a></li>
+                </ul>
+            </div>
+
+            
+            <!-- ========================= -->
+            <!-- ==    CONTACT LINKS    == -->
+            <!-- ========================= -->
+            <div class="col l3 s12">
+                <h5 class="white-text">Contacta con Nosotros</h5>
+                <ul>
+                    <li><a class="white-text" href="#!">Link 1</a></li>
+                    <li><a class="white-text" href="#!">Link 2</a></li>
+                    <li><a class="white-text" href="#!">Link 3</a></li>
+                    <li><a class="white-text" href="#!">Link 4</a></li>
+                </ul>
+            </div>
+
+        </div>
+
+
+        <!-- ========================= -->
+        <!-- ==      LAST LINE      == -->
+        <!-- ========================= -->
+        <div class="footer-copyright">
+            <div class="container">
+                <b>Creado por</b> 
+                <span class="text-lighten-3">CompilandoConocimiento</span>
+            </div>
+        </div>
+
+    </footer>
+
+
+    <!--  +++++++++++++++++++++++++++++++++++++++++++++++++ -->
+    <!--  +++++++++++++++   SCRIPTS     +++++++++++++++++++ -->
+    <!--  +++++++++++++++++++++++++++++++++++++++++++++++++ -->
+    <script src="https://code.jquery.com/jquery-2.1.1.min.js">  </script>
+    <script src="Javascript/Materialize/materialize.js">        </script>
+    <script src="Javascript/Materialize/init.js">               </script>
+
+</BODY>
+</HTML>
